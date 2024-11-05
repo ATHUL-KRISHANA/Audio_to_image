18bff1fdc31f3a5944161bd277b635cd105c9c1f
ATHUL KRISHNA P S <167164847+ATHUL-KRISHANA@users.noreply.github.com>
2024-11-05 11:23:29 +0530

    Audio_to_image

--- audio_final.py
+++ audio_final.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, jsonify, send_file
+from flask import Flask, render_template, request, jsonify
 import pyaudio
 import speech_recognition as sr
+import requests
 from PIL import Image
 from io import BytesIO
-import requests
 import base64
 
 app = Flask(__name__)
@@ -12,14 +12,15 @@ CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000
+DURATION = 10  
 
-def record_audio(duration=10):
+def record_audio():
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
     print("Recording...")
     frames = []
 
-    for _ in range(0, int(RATE / CHUNK * duration)):
+    for _ in range(0, int(RATE / CHUNK * DURATION)):
         data = stream.read(CHUNK)
         frames.append(data)
 
@@ -42,11 +43,10 @@ def recognize_speech(audio_data):
         return f"Could not request results from Google API; {e}"
 
 def get_image_from_colab(text):
-    colab_url = "https://f28c-34-82-106-246.ngrok-free.app/generate_image"  #
+    colab_url = "Replace with your actual URL the public link/generate_image"  # Replace with your actual URL
     response = requests.post(colab_url, data={'text': text})
     if response.status_code == 200:
         image = Image.open(BytesIO(response.content))
-        
         img_io = BytesIO()
         image.save(img_io, 'JPEG')
         img_io.seek(0)
@@ -57,20 +57,27 @@ def get_image_from_colab(text):
 
 @app.route('/')
 def index():
-    return render_template('sam.html')
+    return render_template('audio_final.html')
 
 @app.route('/record', methods=['POST'])
 def record():
-    audio_data = record_audio()  
+    audio_data = record_audio()
     recognized_text = recognize_speech(audio_data)
     if recognized_text:
-        img_base64 = get_image_from_colab(recognized_text)
-        if img_base64:
-            return jsonify({"text": recognized_text, "image": img_base64})
-        else:
-            return jsonify({"error": "Failed to retrieve image from Colab"})
+        return jsonify({"text": recognized_text})
     else:
         return jsonify({"error": "Failed to recognize any text"})
 
+@app.route('/generate_image', methods=['POST'])
+def generate_image():
+    text = request.form.get('text')
+    if text:
+        img_base64 = get_image_from_colab(text)
+        if img_base64:
+            return jsonify({"image": img_base64})
+        else:
+            return jsonify({"error": "Failed to retrieve image from Colab"})
+    return jsonify({"error": "No text provided"})
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8003)
+    app.run(debug=True, port=8000)
